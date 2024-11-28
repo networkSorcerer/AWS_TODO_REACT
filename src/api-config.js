e547c4699ef6d061b1258123a347d80eeb92507a
networkSorcerer <gigattfyhig@gmail.com>
2024-11-28 13:55:17 +0900

    포트번호 변경

--- src/api-config.js
+++ src/api-config.js
@@ -3,7 +3,7 @@ let backendHost;
 const hostname = window && window.location && window.location.hostname;
 
 if (hostname === "localhost") {
-  backendHost = "http://54.180.139.78:8080";
+  backendHost = "http://54.180.139.78:4040";
 }
 // 템플릿 리터럴에서 `${}` 사용
 export const API_BASE_URL = `${backendHost}`;
