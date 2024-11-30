a67d43052cdf7ea84456daf463f6c826a19028e5
networkSocerer <gigattfyhig@gmail.com>
2024-11-30 23:28:55 +0900

    api-config 수정

--- src/api-config.js
+++ src/api-config.js
@@ -1,9 +1,10 @@
 let backendHost;
 
-const hostname = window && window.location && window.location.hostname;
+//const hostname = window && window.location && window.location.hostname;
 
-if (hostname === "localhost") {
-  backendHost = "http://54.180.139.78:4040";
-}
+// if (hostname === "localhost") {
+//   backendHost = "http://54.180.139.78:8080";
+// }
+backendHost = "http://54.180.139.78:8080";
 // 템플릿 리터럴에서 `${}` 사용
 export const API_BASE_URL = `${backendHost}`;
